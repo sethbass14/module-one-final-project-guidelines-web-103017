3183686b3665d10e1ff0dc336d234f6614c7cc70
Jake <jakemacnaughton@gmail.com>
2017-11-15 18:09:42 -0500

    created lib/cli.rb

---
+++ lib/cli.rb
@@ -0,0 +1,19 @@
+class Deadsetter
+
+  def welcome
+    puts "Welcome to Deadsetter, your one-stop shop for every Grateful Dead setlist from 1977-80."
+  end
+
+  def prompt_search
+    puts "How would like to search this heady collection? Please enter a number below:"
+    puts "1. Date"
+    puts "2. Venue"
+    puts "3. City"
+    puts "4. Song"
+    gets.chomp
+
+
+
+
+
+end
