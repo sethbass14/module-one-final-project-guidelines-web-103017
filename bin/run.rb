9fbc181c29549b317dd5713d5115bf798f2225ec
Jake <jakemacnaughton@gmail.com>
2017-11-16 09:43:10 -0500

    working on CLI

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,3 @@
 require_relative '../config/environment'
 
-
-
-  
+  # Deadsetter.new.date_or_year?
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,16 +4,31 @@ class Deadsetter
     puts "Welcome to Deadsetter, your one-stop shop for every Grateful Dead setlist from 1977-80."
   end
 
-  def prompt_search
-    puts "How would like to search this heady collection? Please enter a number below:"
+  def main_prompt
+    puts "How would like to search this heady collection? Please enter 1-3:"
     puts "1. Date"
-    puts "2. Venue"
-    puts "3. City"
-    puts "4. Song"
-    gets.chomp
+    puts "2. Location"
+    puts "3. Song"
+  end
 
+  def gets_input
+    input = gets.chomp
+  end
 
+  def date_prompt
+    puts "Please enter a date (DD-MM-YYYY) or year (YYYY)"
+  end
 
+  def date_or_year?
+    input = gets_input
+    if input.length == 10
+      # call find_show_by_date method
+    elsif input.length == 4
+      # call find_shows_by_year method
+    else
+      # call invalid_input method
+    end
+  end
 
-
+  
 end
